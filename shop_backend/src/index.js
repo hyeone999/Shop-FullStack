3e83db5353b834a18cbf1a89fccf469f79cb79bd
hyeone999 <caps026@naver.com>
2023-05-25 11:09:38 +0900

    feat: express.static()

--- shop_backend/src/index.js
+++ shop_backend/src/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
 const port = 4000;
 
 app.get("/", (req, res) => {
   res.send("안녕하세요, 111");
 });
+
+app.use(express.static(path.join(__dirname, "../uploads")));
 
 app.listen(port, () => {
   console.log(`${port}번에서 실행이 되었습니다.`);
