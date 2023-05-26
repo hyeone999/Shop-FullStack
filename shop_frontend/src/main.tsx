14cf8251d84de68114420f8dc79be827b13c0d6f
hyeone999 <caps026@naver.com>
2023-05-26 18:52:07 +0900

    feat: redux perist 적용

--- shop_frontend/src/main.tsx
+++ shop_frontend/src/main.tsx
@@ -3,12 +3,15 @@ import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./store";
+import { persistor, store } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 );
