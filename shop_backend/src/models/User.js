9866a70350da75cdae93a8f904af64c2d02cb725
hyeone999 <caps026@naver.com>
2023-06-14 09:40:09 +0900

    feat: UserSchema 생성 & model 생성

---
+++ shop_backend/src/models/User.js
@@ -0,0 +1,28 @@
+const { default: mongoose } = require("mongoose");
+
+// 유저 Schema 생성
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    maxLength: 50,
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    minLength: 5,
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+  image: String,
+});
+
+// 모델 생성
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
