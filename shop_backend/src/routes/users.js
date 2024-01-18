714b7fb2f86a8fcf2560079e258305dec913760b
hyeone999 <caps026@naver.com>
2024-01-18 23:13:26 +0900

    feat: 로그인 API 구현 및 jwt 토큰 발급

--- shop_backend/src/routes/users.js
+++ shop_backend/src/routes/users.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
 
 router.post("/register", async (req, res, next) => {
-  // 유저 데이터를 저장
-  // data => data model 이미 구축 완료
   try {
     const user = new User(req.body);
     await user.save();
@@ -12,6 +11,34 @@ router.post("/register", async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+});
+
+router.post("/login", async (req, res, next) => {
+  try {
+    // 유저가 존재하는지 check
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(400).send("Auth failed, email not found");
+    }
+
+    // 비밀번호가 올바른지 check
+    const isMatch = await user.comparePassword(req.body.password);
+    if (!isMatch) {
+      return res.status(400).send("Wrong password");
+    }
+
+    const payload = {
+      userId: user._id.toHexString(),
+    };
+
+    // token을 생성
+    const accessToken = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    return res.json({ user, accessToken });
+  } catch (error) {}
 });
 
 module.exports = router;
