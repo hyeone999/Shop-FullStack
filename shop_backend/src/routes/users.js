e411d4fa9486a683884bce7d59f15981e60dbd35
hyeone999 <caps026@naver.com>
2024-01-19 11:39:42 +0900

    feat: 로그아웃 API 구현

--- shop_backend/src/routes/users.js
+++ shop_backend/src/routes/users.js
@@ -52,4 +52,12 @@ router.post("/login", async (req, res) => {
   } catch (error) {}
 });
 
+router.post("/logout", auth, (req, res, next) => {
+  try {
+    return res.sendStatus(200);
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
