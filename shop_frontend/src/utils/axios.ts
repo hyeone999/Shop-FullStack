4cbb5c178db4670dcb53f9bd3bafb7468e93e19e
hyeone999 <caps026@naver.com>
2024-02-12 16:31:47 +0900

    feat: 토큰 만료시 action 처리

--- shop_frontend/src/utils/axios.ts
+++ shop_frontend/src/utils/axios.ts
@@ -17,4 +17,21 @@ axiosInstance.interceptors.request.use(
   }
 );
 
+// 보편적으로는 로그인 시 refreshToken을 발급 받아 accessToken이 만료될 경우 재발급 받는 경우를 선택하지만
+// 일단 해당 프로젝트에서는 Auth 확인하는 API를 통해 토큰 만료시 강제 로그아웃 처리
+
+// 토큰 만료시 처리하기
+axiosInstance.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    if (error.response.data === "jwt expired") {
+      window.location.reload();
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 export default axiosInstance;
