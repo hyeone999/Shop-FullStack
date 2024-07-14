1b28b101178a9b51915e7a3acc2ba72d6ab938d5
hyeone999 <caps026@naver.com>
2024-07-14 16:10:49 +0900

    feat: product route 구현

--- shop_backend/src/routes/product.js
+++ shop_backend/src/routes/product.js
@@ -26,6 +26,28 @@ router.post("/image", auth, async (req, res, next) => {
   });
 });
 
+router.get("/", async (req, res, next) => {
+  const order = req.query.order ? req.query.order : "desc";
+  const sortBy = req.query.sortBy ? req.query.sortBy : "_id";
+  const limit = req.query.limit ? Number(req.query.limit) : 20;
+  const skip = req.query.skip ? req.query.skip : 0;
+
+  try {
+    const products = await Product.find()
+      .populate("writer")
+      .sort([[sortBy, order]])
+      .skip(skip)
+      .limit(limit);
+
+    const productsTotal = await Product.countDocuments();
+    const hasMore = skip + limit < productsTotal ? true : false;
+
+    return res.status(200).json({ products, hasMore });
+  } catch (error) {
+    next(error);
+  }
+});
+
 router.post("/", auth, async (req, res, next) => {
   try {
     const product = new Product(req.body);
