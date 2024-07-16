45c1b2d143bedc0a15af280cc17e538f6ebe4803
hyeone999 <caps026@naver.com>
2024-07-16 12:09:34 +0900

    feat: filter 기능 구현을 위한 체크박스 컴포넌트 및 기능 구현

--- shop_frontend/src/pages/LandingPage/Sections/CheckBox.tsx
+++ shop_frontend/src/pages/LandingPage/Sections/CheckBox.tsx
@@ -1,5 +1,41 @@
-const CheckBox = () => {
-  return <div>CheckBox</div>;
+import { CheckBoxProps } from "../../../utils/types";
+
+const CheckBox = ({
+  continents,
+  checkedContinents,
+  onFilters,
+}: CheckBoxProps) => {
+  const handleToggle = (continentId: number) => {
+    // 현재 누른 checkbox가 이미 누른 checkbox인지 확인
+    const currentIndex = checkedContinents.indexOf(continentId);
+
+    const newChecked = [...checkedContinents];
+
+    if (currentIndex === -1) {
+      newChecked.push(continentId);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    onFilters(newChecked);
+  };
+
+  return (
+    <div className="p-2 mb-3 bg-gray-100 rounded-md">
+      {continents?.map((continent) => (
+        <div key={continent._id}>
+          <input
+            type="checkbox"
+            onChange={() => handleToggle(continent._id)}
+            checked={
+              checkedContinents.indexOf(continent._id) === -1 ? false : true
+            }
+          />{" "}
+          <label>{continent.name}</label>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default CheckBox;
