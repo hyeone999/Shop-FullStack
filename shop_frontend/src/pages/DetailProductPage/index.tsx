86ecaf0644a925cc9b911ec3c0f9ff5ca5975f0e
hyeone999 <caps026@naver.com>
2025-02-27 17:27:46 +0900

    feat: detail page API 함수 구현 및 UI 틀 구현

--- shop_frontend/src/pages/DetailProductPage/index.tsx
+++ shop_frontend/src/pages/DetailProductPage/index.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axiosInstance from "../../utils/axios";
+import { Product } from "../../utils/types";
 
 const DetailProductPage = () => {
-  return <div>DetailProductPage</div>;
+  const { productId } = useParams();
+  const [product, setProduct] = useState<Product | null>(null);
+
+  useEffect(() => {
+    async function fetchProduct() {
+      try {
+        const response = await axiosInstance.get(
+          `/products/${productId}?type=single`
+        );
+        setProduct(response.data[0]);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchProduct();
+  }, [productId]);
+
+  return (
+    <section>
+      <div className="text-center">
+        <h1 className="p-4 text-2xl">{product?.title}</h1>
+      </div>
+
+      <div className="flex gap-4">
+        <div className="w-1/2">{/* ProductImage */}</div>
+        <div className="w-1/2">{/* ProductInfo */}</div>
+      </div>
+    </section>
+  );
 };
 
 export default DetailProductPage;
