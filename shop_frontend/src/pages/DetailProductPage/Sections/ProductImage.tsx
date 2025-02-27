f13c7c685e4ae97c4d8a05b2480177b7516c9e25
hyeone999 <caps026@naver.com>
2025-02-27 18:02:22 +0900

    feat: ProductImage 컴포넌트 image 기능 구현

--- shop_frontend/src/pages/DetailProductPage/Sections/ProductImage.tsx
+++ shop_frontend/src/pages/DetailProductPage/Sections/ProductImage.tsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react";
 import { ProductProps } from "../../../utils/types";
+import ReactImageGallery from "react-image-gallery";
 
-const ProductImage = (product: ProductProps) => {
-  return <div>ProductImage</div>;
+interface ImageItem {
+  original: string;
+  thumbnail: string;
+}
+const ProductImage = ({ product }: ProductProps) => {
+  const [images, setImages] = useState<ImageItem[]>([]);
+
+  useEffect(() => {
+    if (product?.images && product.images.length > 0) {
+      const images: ImageItem[] = [];
+
+      product?.images.map((imageName) => {
+        return images.push({
+          original: `${import.meta.env.VITE_SERVER_URL}/${imageName}`,
+          thumbnail: `${import.meta.env.VITE_SERVER_URL}/${imageName}`,
+        });
+      });
+      setImages(images);
+    }
+  }, [product]);
+
+  console.log(images);
+
+  return <ReactImageGallery items={images} />;
 };
 
 export default ProductImage;
